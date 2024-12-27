ac0a743d7258243c90b413e8bef9a33138d3c39f
youssef <labrachyoussef1@gmail.com>
2024-12-27 01:00:23 +0100

    user service

---
+++ src/main/java/isme/service_user/ServiceUserApplication.java
@@ -0,0 +1,16 @@
+package isme.service_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUserApplication.class, args);
+    }
+
+}
